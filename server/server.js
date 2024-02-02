b6b1926d8c145469120136d3e8f37600fe4efd07
AlexDev_ <alessandrodalfovo2003@gmail.com>
2024-02-02 22:40:25 +0100

    Test deploy

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ const {app, port, config} = require("./app.js");
 
 http.createServer(app)
     .listen(port, () => {
-        console.log(`Listening on ${config.baseURL}`);
+        console.log(`Inizializzazione completata. Server in ascolto su: ${config.baseURL}`);
     });
